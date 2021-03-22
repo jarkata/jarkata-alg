a94a06839b98a6d3dbf7417d5c18d4c4c38083fc
vkata <fx.windy@foxmail.com>
2021-03-22 23:29:48 +0800

    init

---
+++ alg-leetcode/src/main/java/cn/jarkata/alg/leetcode/App.java
@@ -0,0 +1,13 @@
+package cn.jarkata.alg.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
