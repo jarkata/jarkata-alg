15ba6a6e56eac48cdfc53316da651a406d76325c
vkata <fx.windy@foxmail.com>
2021-04-12 00:17:09 +0800

    init

--- alg-leetcode/src/main/java/cn/jarkata/alg/leetcode/Solution.java
+++ alg-leetcode/src/main/java/cn/jarkata/alg/leetcode/Solution.java
@@ -1,30 +1,52 @@
 package cn.jarkata.alg.leetcode;
 
+import java.util.Arrays;
+
 /**
  * 把只包含质因子2，3和5的数称作丑数（Ugly Number）
  * <p>
  * 质因数（素因数或质因子）在数论里是指能整除给定正整数的质数。
  * 1与任何正整数（包括1本身）都是互质
  * 除了1以外，两个没有其他共同质因子的正整数称为互质
+ * <a href="https://leetcode-cn.com/problems/ugly-number-ii/">264. 丑数 II</a>
  */
 public class Solution {
 
     public int nthUglyNumber(int n) {
         int no = 0;
         int count = 0;
+        int minVal = 1;
         while (true) {
             if (count == n) {
-                return no;
+                return minVal;
             }
-            no++;
-            if (no == 1) {
-                count++;
-                continue;
+            int[] tmp = new int[3];
+            if (minVal == 1) {
+                tmp[0] = minVal * 2;
+                tmp[1] = minVal * 3;
+                tmp[2] = minVal * 5;
+            } else {
+
             }
-            if (no % 2 == 0 || no % 3 == 0 || no % 5 == 0) {
-                count++;
-            }
+
+
+            Arrays.sort(tmp);
+            minVal = tmp[0];
+            System.out.println(minVal);
+            count++;
         }
+    }
+
+
+    public static boolean isPrime(int n) {
+        if (n <= 3) {
+            return n > 1;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
